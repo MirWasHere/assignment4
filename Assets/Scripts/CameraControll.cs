fe7a3722420667cd7ef82f099eeb27c994c97d07
MirWasHere <mirwashere@users.noreply.github.com>
2024-12-04 23:31:04 -0500

    Added interactable tree

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -8,23 +8,42 @@ public class CameraController : MonoBehaviour
     public GameObject player;
 
     private Vector3 offset;
+    
+    // Keeps track of how many times the camera has changed
+    // to prevent a mess
+    private int tracker;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        tracker = 0;
         offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+
         transform.position = player.transform.position + offset;
-        // if(SceneManager.GetActiveScene().name == "forestAndTown")
+
+        // if(SceneManager.GetActiveScene().name == "forestAndTown" && tracker < 3)
         // {
-        //     transform.eulerAngles.y = player.transform.eulerAngles.y;
-        //     transform.eulerAngles.x = player.transform.eulerAngles.x;
-        //     transform.eulerAngles.z = player.transform.eulerAngles.z;
+        //     Debug.Log("New Scene, New Camera Angle");
+        //     transform.Rotate(15, -90, 15);
+        //     transform.position = new Vector3(140f, 15f, -82f);
+
+        //     Debug.Log("Camera x: " + transform.position.x);
+        //     tracker ++;
         // }
+        // else if(SceneManager.GetActiveScene().name != "forestAndTown")
+        // {
+        //     transform.position = player.transform.position + offset;
+        // }
+        // else
+        // {
+        //     transform.position = player.transform.position + offset;
+        // }
+        
     }
 }
